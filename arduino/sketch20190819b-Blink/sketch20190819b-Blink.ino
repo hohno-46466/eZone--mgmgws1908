191c12b1e84f081c0f0890bd239c814d72e7e502
hohno-46466 <hohno.46466@gmail.com>
2019-08-19 07:25:36 +0900

    updated by hohno for mgmgws1908

--- arduino/sketch20190819b-Blink/sketch20190819b-Blink.ino
+++ arduino/sketch20190819b-Blink/sketch20190819b-Blink.ino
@@ -4,18 +4,20 @@
 
 // ---------------------------------------------------------
 
+// 初期設定
+
 void setup() {
 
   Serial.begin(57600);
 
   pinMode(13, OUTPUT);
 
-  // 以下の3行は「ボタン・シールド」のための設定
+  // 以下の3行は「ボタンシールド」のための設定
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
 
-  // 以下の 3行も「ボタン・シールド」のための設定
+  // 以下の 3行も「ボタンシールド」のための設定
   pinMode(A5, INPUT_PULLUP);
   pinMode(A4, INPUT_PULLUP);
   pinMode(A3, INPUT_PULLUP);
@@ -24,22 +26,30 @@ void setup() {
 
 // ---------------------------------------------------------
 
+// メインループ
+
 void loop() {
 
+  // Arduino 本体のLEDを点灯
   digitalWrite(13, HIGH);
 
+  //「ボタンシールド」の3つのLEDを点灯
   digitalWrite(9,  HIGH);
   digitalWrite(10, HIGH);
   digitalWrite(11, HIGH);
 
+  // 200ミリ秒待つ
   delay(200);
 
+  // Arduino 本体のLEDを消灯
   digitalWrite(13, LOW);
 
+  //「ボタンシールド」の3つのLEDを消灯
   digitalWrite(9,  LOW);
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
 
+  // 200ミリ秒待つ
   delay(200);
 
 }
