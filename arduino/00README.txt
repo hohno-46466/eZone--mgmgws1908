b64c2e1114f4e435f0745ba622a6d5693522c39d
hohno-46466 <hohno.46466@gmail.com>
2019-08-19 07:03:10 +0900

    updated by hohno for mgmgws1908

---
+++ arduino/00README.txt
@@ -0,0 +1,7 @@
+# 00README.txt
+
+# Last update: Mon Aug 19 07:00:45 JST 2019
+
+このフォルダには、集中講義で利用する Arduino のスケッチを順次追記してゆく
+
+@hohno_at_kuimc
---
+++ arduino/sketch20190819b-Blink/sketch20190819b-Blink.ino
@@ -0,0 +1,12 @@
+
+// sketch20190819a_Blank.ino
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
