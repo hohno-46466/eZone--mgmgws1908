26f3f5cce7fbc1a89c85f4412d23163ed0fce90a
hohno-46466 <hohno.46466@gmail.com>
2019-08-19 07:21:31 +0900

    updated by hohno for mgmgws1908

--- arduino/sketch20190819a_00_Blank/sketch20190819a_00_Blank.ino
+++ arduino/sketch20190819a_00_Blank/sketch20190819a_00_Blank.ino
@@ -1,12 +1,19 @@
+//
+// sketch20190819a_00_Blank.ino
+//
 
-// sketch20190819a_Blank.ino
+// ---------------------------------------------------------
 
 void setup() {
   // put your setup code here, to run once:
 
 }
 
+// ---------------------------------------------------------
+
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+// ---------------------------------------------------------
--- arduino/sketch20190819b-Blink/sketch20190819b-Blink.ino
+++ arduino/sketch20190819b-Blink/sketch20190819b-Blink.ino
@@ -1,12 +1,47 @@
-
+//
 // sketch20190819a_Blank.ino
+//
+
+// ---------------------------------------------------------
 
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(57600);
+
+  pinMode(13, OUTPUT);
+
+  // 以下の3行は「ボタン・シールド」のための設定
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
+  // 以下の 3行も「ボタン・シールド」のための設定
+  pinMode(A5, INPUT_PULLUP);
+  pinMode(A4, INPUT_PULLUP);
+  pinMode(A3, INPUT_PULLUP);
 
 }
+
+// ---------------------------------------------------------
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  digitalWrite(13, HIGH);
+
+  digitalWrite(9,  HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+
+  delay(200);
+
+  digitalWrite(13, LOW);
+
+  digitalWrite(9,  LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+
+  delay(200);
 
 }
+
+// ---------------------------------------------------------
