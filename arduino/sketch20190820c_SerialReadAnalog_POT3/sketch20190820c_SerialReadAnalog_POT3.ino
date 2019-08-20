336ae677d99d27336020bd5a0984dc4de8be8e5a
hohno-46466 <hohno.46466@gmail.com>
2019-08-20 12:58:39 +0900

    updated by hohno

---
+++ arduino/sketch20190820c_SerialReadAnalog_POT3/sketch20190820c_SerialReadAnalog_POT3.ino
@@ -0,0 +1,53 @@
+//
+// sketch20190820c_SerialReadAnalog_POT3.ino
+//
+// Written by @hohno_at_kuimc
+//
+// Public domain
+//
+// Last update: Mon Aug 19 07:28:04 JST 2019 by @hohno_at_kuimc
+//
+
+const int redPin = 11;
+const int greenPin = 10;
+const int bluePin = 9;
+
+// ---------------------------------------------------------
+
+void setup() {
+  // put your setup code here, to run once:
+
+   Serial.begin(57600);
+   pinMode(redPin, OUTPUT);
+   pinMode(greenPin, OUTPUT);
+   pinMode(bluePin, OUTPUT);
+}
+
+// ---------------------------------------------------------
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+	while (Serial.available() > 0) {
+      int r = Serial.parseInt();
+      int g = Serial.parseInt();
+      int b = Serial.parseInt();
+
+			char c;
+      while (((c = Serial.read()) != '\n') && (c != '\r')) {}   // 行末まで読み飛ばす
+
+      red   = 255 - constrain(r, 0, 255);
+      green = 255 - constrain(g, 0, 255);
+      blue  = 255 - constrain(b, 0, 255);
+
+      analogWrite(redPin, red);
+      analogWrite(greenPin, green);
+      analogWrite(bluePin, blue);
+
+      Serial.print(r); Serial.print(",");
+      Serial.print(g); Serial.print(",");
+      Serial.println(b);
+   }
+}
+
+// ---------------------------------------------------------
