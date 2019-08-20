33f2663721b71747e596a1f7f0bf362760e44156
hohno-46466 <hohno.46466@gmail.com>
2019-08-20 12:43:28 +0900

    updated by hohno

---
+++ arduino/sketch20190820a_AnalogReadSerial_POT1/sketch20190820a_AnalogReadSerial1.ino
@@ -0,0 +1,32 @@
+//
+// sketch20190820a_AnalogReadSerial_POT1.ino
+//
+// Written by @hohno_at_kuimc
+//
+// Public domain
+//
+// Last update: Mon Aug 19 07:28:04 JST 2019 by @hohno_at_kuimc
+//
+
+#define VOL (A0)
+
+int valVR;
+
+// ---------------------------------------------------------
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+}
+
+// ---------------------------------------------------------
+
+void loop() {
+  // put your main code here, to run repeatedly:
+	valVR = map(analogRead(VOL), 0, 1023, 0, 255);
+	// Serial.write(77); Serial.write(valVR);
+  Serial.println(valVR);
+  delay(100);
+}
+
+// ---------------------------------------------------------
